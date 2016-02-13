d6d0bdc3ccc57571af6aedb478a15a03edff9864
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 06:12:12 -0800

    add comments to playground

--- Example/Visual.playground/Pages/CoordinatedSpace.xcplaygroundpage/Contents.swift
+++ Example/Visual.playground/Pages/CoordinatedSpace.xcplaygroundpage/Contents.swift
@@ -5,15 +5,16 @@
 import UIKit
 let screen = UIScreen.mainScreen()
 
-
 let inner = UIView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
 inner.backgroundColor = UIColor.redColor()
+//: Unplaced view frame equal bounds on screen
 inner.frame == inner.convertRect(inner.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
 
 let outer = UIView(frame: CGRect(x: 5, y: 5, width: 40, height: 40))
+//: Unplaced view frame equal bounds on screen
 outer.frame == outer.convertRect(outer.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
 outer.backgroundColor = UIColor.greenColor()
-
+//: Inner view bounds in space of outer view is frame on screen
 inner.convertRect(inner.bounds, toCoordinateSpace: outer)
 
 outer.addSubview(inner)
@@ -21,10 +22,13 @@ outer.addSubview(inner)
 inner.convertPoint(CGPointZero, toCoordinateSpace: outer)
 inner.bounds
 
+//: Inner view bounds in space of outer view equal frame
 inner.frame == inner.convertRect(inner.bounds, toCoordinateSpace: outer)
 
-inner.convertRect(inner.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
-
+//: Outer view frame is in coordinates of screen
 outer.convertRect(outer.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
+
+//: Inner view on screen makes sense
+inner.convertRect(inner.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
 
 //: [Next](@next)
