f2b604b182a71fcbe725eaebca8273435b75c03e
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 03:54:08 -0800

    add ImageViewSpace

--- ImageCoordinateSpace/UIImageView+UICoordinatedSpace.swift
+++ ImageCoordinateSpace/UIImageView+UICoordinatedSpace.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
+public class ImageViewSpace : NSObject, UICoordinateSpace {
+    var view : UIImageView
+    public var bounds: CGRect
+
+    init(view v: UIImageView) {
+        view = v
+        bounds = v.bounds
+        super.init()
+    }
+
+    public func convertPoint(point: CGPoint, toCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGPoint {
+        return CGPointZero
+    }
+
+    public func convertPoint(point: CGPoint, fromCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGPoint {
+        return CGPointZero
+    }
+
+    public func convertRect(rect: CGRect, toCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGRect {
+        return CGRectZero
+    }
+
+    public func convertRect(rect: CGRect, fromCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGRect {
+        return CGRectZero
+    }
+
+}
+
 public extension UIImageView {
     func imageCoordinatedSpace() -> UICoordinateSpace {
-        return self
+        return ImageViewSpace(view: self)
     }
 }
