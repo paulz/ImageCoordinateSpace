c9947d3535c94e5efab763634eae390f2786190f
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 04:06:05 -0800

    make first spec to pass

--- ImageCoordinateSpace/UIImageView+UICoordinatedSpace.swift
+++ ImageCoordinateSpace/UIImageView+UICoordinatedSpace.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 public class ImageViewSpace : NSObject, UICoordinateSpace {
-    var view : UIImageView
+    var imageView : UIImageView
     public var bounds: CGRect
 
     init(view v: UIImageView) {
-        view = v
+        imageView = v
         bounds = v.bounds
         super.init()
     }
 
     public func convertPoint(point: CGPoint, toCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGPoint {
-        return CGPointZero
+        let start = imageView.convertPoint(point, fromCoordinateSpace: coordinateSpace)
+        let imageSize = imageView.image!.size;
+        let viewSize  = coordinateSpace.bounds.size;
+        let ratioX = viewSize.width / imageSize.width
+        let ratioY = viewSize.height / imageSize.height
+        let scale = min(ratioX, ratioY);
+
+        let imagePoint = start
+
+        var viewPoint = imagePoint
+        viewPoint.x *= scale;
+        viewPoint.y *= scale;
+
+        viewPoint.x += (viewSize.width  - imageSize.width  * scale) / 2;
+        viewPoint.y += (viewSize.height  - imageSize.height  * scale) / 2;
+
+        return viewPoint
     }
 
     public func convertPoint(point: CGPoint, fromCoordinateSpace coordinateSpace: UICoordinateSpace) -> CGPoint {
