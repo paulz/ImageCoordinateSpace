ad3f3e66d6a524feaae132c4fa6465d0d545b810
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 05:04:38 -0800

    add sandbox page for aspect fit

--- ImageCoordinateSpace/SandBox.playground/Contents.swift
+++ ImageCoordinateSpace/SandBox.playground/Contents.swift
@@ -2,14 +2,20 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
+//: Aspect Fit
 let image = UIImage(named: "rose.jpg")!
-print(image)
-
-
 let imageView = UIImageView(image: image)
 let square = CGSize(width: 100, height: 100)
 imageView.bounds = CGRect(origin: CGPointZero, size: square)
+imageView.backgroundColor = UIColor.greenColor()
 imageView.contentMode = .ScaleAspectFit
-imageView.imageCoordinatedSpace()
+let imageSpace = imageView.imageCoordinatedSpace()
+let topLeft = imageSpace.convertPoint(CGPointZero, toCoordinateSpace: imageView)
+image.size
+let bottomRight = CGPoint(x: image.size.width, y: image.size.height)
+let lowerRight = imageSpace.convertPoint(bottomRight, toCoordinateSpace: imageView)
+let bottomMargin = imageView.bounds.height - lowerRight.y
+let topMargin  = topLeft.y
+//: margins should be the same
+topMargin == bottomMargin
+
