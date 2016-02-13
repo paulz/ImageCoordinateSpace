34567fe55dc21ba4097392c63c5eeed6470cbf6a
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 03:20:57 -0800

    add spec for aspect fit

--- ImageCoordinateSpaceTests/UIImageView+ImageCoordinatedSpaceSpec.swift
+++ ImageCoordinateSpaceTests/UIImageView+ImageCoordinatedSpaceSpec.swift
@@ -3,16 +3,20 @@ import Nimble
 
 class UIImageView_ImageCoordinatedSpaceSpec: QuickSpec {
     override func spec() {
-        let image = UIImage(named: "rose")
+        let image = UIImage(named: "rose")!
         let imageView = UIImageView(image: image)
 
         describe("view UICoordinateSpace") {
             context("same space") {
+                let randomPoint = CGPoint(x: random(), y: random())
+                let randomSize = CGSize(width: random(), height: random())
+                let randomRect = CGRect(origin: randomPoint, size: randomSize)
+
                 it("should not change") {
-                    expect(imageView.convertPoint(CGPointZero, fromCoordinateSpace: imageView)) == CGPointZero
-                    expect(imageView.convertPoint(CGPointZero, toCoordinateSpace: imageView)) == CGPointZero
-                    expect(imageView.convertRect(CGRectZero, fromCoordinateSpace: imageView)) == CGRectZero
-                    expect(imageView.convertRect(CGRectZero, toCoordinateSpace: imageView)) == CGRectZero
+                    expect(imageView.convertPoint(randomPoint, fromCoordinateSpace: imageView)) == randomPoint
+                    expect(imageView.convertPoint(randomPoint, toCoordinateSpace: imageView)) == randomPoint
+                    expect(imageView.convertRect(randomRect, fromCoordinateSpace: imageView)) == randomRect
+                    expect(imageView.convertRect(randomRect, toCoordinateSpace: imageView)) == randomRect
                 }
             }
         }
@@ -25,6 +29,35 @@ class UIImageView_ImageCoordinatedSpaceSpec: QuickSpec {
                     expect(imageSpace.convertPoint(CGPointZero, toCoordinateSpace: imageView)) == CGPointZero
                     expect(imageSpace.convertRect(CGRectZero, fromCoordinateSpace: imageView)) == CGRectZero
                     expect(imageSpace.convertRect(CGRectZero, toCoordinateSpace: imageView)) == CGRectZero
+                }
+            }
+
+            context("aspect fit") {
+                beforeEach {
+                    let square = CGSize(width: 100, height: 100)
+                    imageView.bounds = CGRect(origin: CGPointZero, size: square)
+                    imageView.contentMode = .ScaleAspectFit
+                }
+
+                it("should be within the view") {
+                    let imageSpace = imageView.imageCoordinatedSpace()
+                    let imageSize = image.size;
+                    let viewSize  = imageView.bounds.size;
+                    let ratioX = viewSize.width / imageSize.width
+                    let ratioY = viewSize.height / imageSize.height
+                    let scale = min(ratioX, ratioY);
+
+                    let imagePoint = CGPointZero
+
+                    var viewPoint = imagePoint
+                    viewPoint.x *= scale;
+                    viewPoint.y *= scale;
+
+                    viewPoint.x += (viewSize.width  - imageSize.width  * scale) / 2;
+                    viewPoint.y += (viewSize.height  - imageSize.height  * scale) / 2;
+
+                    expect(imageSpace.convertPoint(CGPointZero, toCoordinateSpace: imageView)) == viewPoint
+
                 }
             }
         }
