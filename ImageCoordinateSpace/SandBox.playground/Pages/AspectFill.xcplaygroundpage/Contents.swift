8eb18b78ff1431139577ccc9bf0dc4dda20403d8
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 05:24:30 -0800

    add aspect fill page

---
+++ ImageCoordinateSpace/SandBox.playground/Pages/AspectFill.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import UIKit
+
+//: Aspect Fill
+let image = UIImage(named: "rose.jpg")!
+let imageView = UIImageView(image: image)
+let square = CGSize(width: 100, height: 100)
+imageView.bounds = CGRect(origin: CGPointZero, size: square)
+imageView.backgroundColor = UIColor.greenColor()
+imageView.contentMode = .ScaleAspectFill
+let imageSpace = imageView.imageCoordinatedSpace()
+let topLeft = imageSpace.convertPoint(CGPointZero, toCoordinateSpace: imageView)
+image.size
+let bottomRight = CGPoint(x: image.size.width, y: image.size.height)
+let lowerRight = imageSpace.convertPoint(bottomRight, toCoordinateSpace: imageView)
+let bottomMargin = imageView.bounds.height - lowerRight.y
+let topMargin  = topLeft.y
+//: margins should be the same
+topMargin == bottomMargin
+
+
+//: [Next](@next)
