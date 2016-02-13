3c37e7836d2382015cb58e505b6b0cbe066255e4
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 14:11:17 -0800

    add page for right

--- Example/Visual.playground/Pages/Right.xcplaygroundpage/Contents.swift
+++ Example/Visual.playground/Pages/Right.xcplaygroundpage/Contents.swift
@@ -2,18 +2,23 @@
 
 import UIKit
 import CoordinatedSpace
-//: Center
+//: Right
 let image = UIImage(named: "rose.jpg")!
 let imageView = UIImageView(image: image)
 let square = CGSize(width: 200, height: 200)
 imageView.bounds = CGRect(origin: CGPointZero, size: square)
 imageView.backgroundColor = UIColor.greenColor()
-imageView.contentMode = .Center
+imageView.contentMode = .Right
 let imageSpace = imageView.imageCoordinatedSpace()
 let topLeft = imageSpace.convertPoint(CGPointZero, toCoordinateSpace: imageView)
 image.size
+//: margin should be on the left only
+assert(topLeft.x == square.width - image.size.width)
+
 let bottomRight = CGPoint(x: image.size.width, y: image.size.height)
 let lowerRight = imageSpace.convertPoint(bottomRight, toCoordinateSpace: imageView)
+//: top margin should equal bottom margin
+assert(square.height - lowerRight.y == topLeft.y)
 
 //: image should not be stretched
 assert(lowerRight.x - topLeft.x == image.size.width)
