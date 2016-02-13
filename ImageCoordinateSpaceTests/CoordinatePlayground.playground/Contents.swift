f94ff142a66c52e2c1f987fc28aa5ea519fa00d3
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 04:19:24 -0800

    add playground

---
+++ ImageCoordinateSpaceTests/CoordinatePlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let screen = UIScreen.mainScreen()
+
+
+let inner = UIView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
+inner.backgroundColor = UIColor.redColor()
+inner.frame == inner.convertRect(inner.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
+
+let outer = UIView(frame: CGRect(x: 5, y: 5, width: 40, height: 40))
+outer.frame == outer.convertRect(outer.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
+outer.backgroundColor = UIColor.greenColor()
+
+inner.convertRect(inner.bounds, toCoordinateSpace: outer)
+
+outer.addSubview(inner)
+
+inner.convertPoint(CGPointZero, toCoordinateSpace: outer)
+inner.bounds
+
+inner.frame == inner.convertRect(inner.bounds, toCoordinateSpace: outer)
+
+inner.convertRect(inner.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
+
+outer.convertRect(outer.bounds, toCoordinateSpace: screen.fixedCoordinateSpace)
