c272e002182be583a4d16ab6b0460c15201b4ce4
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-13 04:56:44 -0800

    add source to sandbox

--- ImageCoordinateSpace/SandBox.playground/Contents.swift
+++ ImageCoordinateSpace/SandBox.playground/Contents.swift
@@ -7,3 +7,9 @@ var str = "Hello, playground"
 let image = UIImage(named: "rose.jpg")!
 print(image)
 
+
+let imageView = UIImageView(image: image)
+let square = CGSize(width: 100, height: 100)
+imageView.bounds = CGRect(origin: CGPointZero, size: square)
+imageView.contentMode = .ScaleAspectFit
+imageView.imageCoordinatedSpace()
