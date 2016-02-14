0f194a108c3ed3fa586b6f4c4c88bb4624d0b8f3
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-14 06:28:24 -0800

    verify rect reverse

--- CoordinatedSpaceTests/ReverseConversionSpec.swift
+++ CoordinatedSpaceTests/ReverseConversionSpec.swift
@@ -29,7 +29,7 @@ class ReverseConversionSpec: QuickSpec {
                 expect(imageView.imageCoordinatedSpace().convertPoint(imagePoint, toCoordinateSpace: imageView), file:file, line: line) == viewPoint
             }
 
-            fcontext("point") {
+            context("point") {
                 it("should revert to original point") {
                     imageView.contentMode = .ScaleAspectFit
                     let viewPoint = imageView.imageCoordinatedSpace().convertPoint(imagePoint, toCoordinateSpace: imageView)
@@ -47,6 +47,39 @@ class ReverseConversionSpec: QuickSpec {
                             let point = imageView.imageCoordinatedSpace().convertPoint(viewPoint, fromCoordinateSpace: imageView)
                             expect(point) == imagePoint
                         }
+                    }
+                }
+            }
+            
+            context("any rect") {
+                var randomRect : CGRect!
+
+                func smallRandom() -> Int {
+                    return random() % 100
+                }
+
+                beforeEach {
+                    randomRect = CGRect(origin: CGPoint(x: smallRandom(), y: smallRandom()), size: CGSize(width: smallRandom(), height: smallRandom()))
+                }
+
+                func beVeryCloseTo(expectedValue: CGRect!) -> MatcherFunc <CGRect> {
+                    return MatcherFunc { actualExpression, failureMessage in
+                        failureMessage.postfixMessage = "equal <\(expectedValue)>"
+                        let actual = try actualExpression.evaluate()!
+                        let delta : CGFloat = 0.0000000000001
+                        return actual.origin.x - expectedValue.origin.x < delta &&
+                            actual.origin.y - expectedValue.origin.y < delta &&
+                            actual.size.width - expectedValue.size.width < delta &&
+                            actual.size.height - expectedValue.size.height < delta
+                    }
+                }
+
+                for var mode = UIViewContentMode.ScaleToFill.rawValue; mode <= UIViewContentMode.BottomRight.rawValue; mode++ {
+                    it("in mode \(mode) should reverse to original") {
+                        imageView.contentMode = UIViewContentMode(rawValue: mode)!
+                        let viewRect = imageView.imageCoordinatedSpace().convertRect(randomRect, toCoordinateSpace: imageView)
+                        let imageRect = imageView.imageCoordinatedSpace().convertRect(viewRect, fromCoordinateSpace: imageView)
+                        expect(imageRect).to(beVeryCloseTo(randomRect))
                     }
                 }
             }
