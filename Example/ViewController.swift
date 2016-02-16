d360ed9b3cb527601015f2dba3f4baddcb020e8a
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-15 20:06:59 -0800

    animate overlay to the correct placement

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,22 +7,34 @@
 //
 
 import UIKit
+import ImageCoordinateSpace
 
 class ViewController: UIViewController {
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var overlayImageView: UIImageView!
+    let placement = CGRect(x: 321/2, y: 102/2, width: 63/2, height: 64/2)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func updateOvelayPosition() {
+        overlayImageView.frame = backgroundImageView.imageCoordinateSpace.convertRect(placement, toCoordinateSpace: view)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        updateOvelayPosition()
+    }
+
+    func nextContentMode() {
+        backgroundImageView.contentMode = UIViewContentMode(rawValue: backgroundImageView.contentMode.rawValue + 1)!
     }
 
     @IBAction func didTap(sender: AnyObject) {
+        nextContentMode()
+        if backgroundImageView.contentMode == .Redraw {
+            nextContentMode()
+        }
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.updateOvelayPosition()
+        }
     }
 
 }
