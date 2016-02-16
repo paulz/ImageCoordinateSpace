b722cdf0986b0c017c038d404e0cda70b35fd13f
Paul Zabelin <pzabelin+git@gmail.com>
2016-02-15 19:51:08 -0800

    add overlay to storyboard

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var overlayImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTap(sender: AnyObject) {
+    }
 
 }
 
