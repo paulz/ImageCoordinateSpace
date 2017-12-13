4d24a06b35846c7a0c21dadaf1f094b7343be13b
Paul Zabelin <paul@omadahealth.com>
2017-12-13 08:41:28 -0800

    fix deprecated warnings in specs
    migrate matcher functions to Nimble predicates

--- Unit Tests/ApproximateMatchers.swift
+++ Unit Tests/ApproximateMatchers.swift
@@ -9,24 +9,26 @@
 import Nimble
 import UIKit
 
-func beVeryCloseTo(_ expectedValue: CGRect!) -> MatcherFunc <CGRect> {
-    return MatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "equal <\(expectedValue)>"
+func beVeryCloseTo(_ expectedValue: CGRect!) -> Predicate <CGRect> {
+    return Predicate.simpleNilable("equal <\(expectedValue.debugDescription)>", matcher: { actualExpression -> PredicateStatus in
         let actual = try actualExpression.evaluate()!
         let delta : CGFloat = 0.000002
-        return abs(actual.origin.x - expectedValue.origin.x) < delta &&
+        return PredicateStatus(bool:
+            abs(actual.origin.x - expectedValue.origin.x) < delta &&
             abs(actual.origin.y - expectedValue.origin.y) < delta &&
             abs(actual.size.width - expectedValue.size.width) < delta &&
             abs(actual.size.height - expectedValue.size.height) < delta
-    }
+        )
+    })
 }
 
-func beVeryCloseTo(_ expectedValue: CGPoint!) -> MatcherFunc <CGPoint> {
-    return MatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "equal <\(expectedValue)>"
+func beVeryCloseTo(_ expectedValue: CGPoint!) -> Predicate <CGPoint> {
+    return Predicate.simpleNilable("equal <\(expectedValue.debugDescription)>", matcher: { actualExpression -> PredicateStatus in
         let actual = try actualExpression.evaluate()!
         let delta : CGFloat = 0.000002
-        return abs(actual.x - expectedValue.x) < delta &&
+        return PredicateStatus(bool:
+            abs(actual.x - expectedValue.x) < delta &&
             abs(actual.y - expectedValue.y) < delta
-    }
+        )
+    })
 }
