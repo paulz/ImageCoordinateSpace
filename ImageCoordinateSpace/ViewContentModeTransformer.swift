9277ea386eca5e5b20601057d9fc8768f7007cf5
Paul Zabelin <pzabelin+git@gmail.com>
2018-03-12 16:04:33 -0700

    refactor ViewContentModeTransformer into struct

--- ImageCoordinateSpace/ViewContentModeTransformer.swift
+++ ImageCoordinateSpace/ViewContentModeTransformer.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewContentModeTransformer {
+struct ViewContentModeTransformer {
     let viewSize : CGSize
     let contentSize : CGSize
     let contentMode : UIViewContentMode
@@ -19,7 +19,9 @@ class ViewContentModeTransformer {
         contentMode = mode
     }
     
-    private lazy var scaleToFill = CGAffineTransform(scaleTo: viewSize, from: contentSize)
+    private func scaleToFill() -> CGAffineTransform {
+        return CGAffineTransform(scaleTo: viewSize, from: contentSize)
+    }
 
     private func translate(_ byX:ScaleFactor, _ byY:ScaleFactor, sizeScale scale:CGFloat = 1.0) -> CGAffineTransform {
         let x = byX.scale(value: viewSize.width - contentSize.width * scale)
@@ -28,7 +30,8 @@ class ViewContentModeTransformer {
     }
     
     private func translateAndScale(using reduceFunction:(CGFloat,CGFloat)->CGFloat) -> CGAffineTransform {
-        let scale = reduceFunction(scaleToFill.scaleX, scaleToFill.scaleY)
+        let fill = scaleToFill()
+        let scale = reduceFunction(fill.scaleX, fill.scaleY)
         return translate(.half, .half, sizeScale: scale).scaledBy(x: scale, y: scale)
     }
     
@@ -39,7 +42,7 @@ class ViewContentModeTransformer {
         case .scaleAspectFit:
             return translateAndScale(using: min)
         case .scaleToFill, .redraw:
-            return scaleToFill
+            return scaleToFill()
         case .topLeft:
             return CGAffineTransform.identity
         case .center:
