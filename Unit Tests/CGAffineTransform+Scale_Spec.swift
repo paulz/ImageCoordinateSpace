ca9116b6eb6e0943ce26189cf42ad3bb520cc6e9
Paul Zabelin <pzabelin+git@gmail.com>
2018-10-18 10:33:16 -0700

    add CGAffineTransform_Scale_Spec

---
+++ Unit Tests/CGAffineTransform+Scale_Spec.swift
@@ -0,0 +1,32 @@
+import Quick
+import Nimble
+@testable import ImageCoordinateSpace
+
+class CGAffineTransform_Scale_Spec: QuickSpec {
+    override func spec() {
+        describe(CGAffineTransform.self) {
+            context(\CGAffineTransform.scaleX) {
+                it("should be multiplier for X axis") {
+                    let transform = CGAffineTransform(scaleX: 123.456, y: CGFloat.nextRandom())
+                    expect(transform.scaleX) == 123.456
+                }
+            }
+
+            context(\CGAffineTransform.scaleY) {
+                it("should be multiplier for Y axis") {
+                    let transform = CGAffineTransform(scaleX: CGFloat.nextRandom(), y: 987.654)
+                    expect(transform.scaleY) == 987.654
+                }
+            }
+
+            context(CGAffineTransform.init(scaleTo:from:)) {
+                it("should create tranform that will scale from one size to another") {
+                    let from = CGSize.nextRandom()
+                    let to = CGSize.nextRandom()
+                    let transform = CGAffineTransform(scaleTo: to, from: from)
+                    expect(from.applying(transform)) == to
+                }
+            }
+        }
+    }
+}
