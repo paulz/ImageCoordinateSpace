96d70ef9d18cc0596bbd9865e2ed4e5788539f2b
Paul Zabelin <pzabelin+git@gmail.com>
2019-03-10 22:40:56 -0700

    refactor CGAffineTransformInvertSpec

--- Unit Tests/CGAffineTransformInvertSpec.swift
+++ Unit Tests/CGAffineTransformInvertSpec.swift
@@ -3,37 +3,23 @@ import Nimble
 
 internal class CGAffineTransformInvertSpec: QuickSpec {
     override func spec() {
-        describe("invert transform") {
-            var modified = CGAffineTransform.identity
-
-            afterEach {
-                let invert = modified.inverted()
-                let reverted = modified.concatenating(invert)
+        sharedExamples("reverts to identity") { sharedContext in
+            it("should be identity") {
+                let transform = sharedContext()["transform"] as! CGAffineTransform
+                let invert = transform.inverted()
+                let reverted = transform.concatenating(invert)
                 expect(reverted) ≈ CGAffineTransform.identity
             }
+        }
 
+        describe("any transform become identity") {
             context("concatenating inverted") {
-                it("should revert scale") {
-                    modified = CGAffineTransform(scaleX: 2, y: 3)
-                }
-
-                it("should revert translate") {
-                    modified = CGAffineTransform(translationX: 100, y: 200)
-                }
-
-                it("should revert translate and scale") {
-                    let translation = CGAffineTransform(translationX: 100, y: 200)
-                    modified = translation.scaledBy(x: 2, y: 3)
-                }
-
-                it("should revert scale and translate") {
-                    let scale = CGAffineTransform(scaleX: 2, y: 3)
-                    modified = scale.translatedBy(x: 200, y: 300)
-                }
-
-                it("should revert random transform") {
-                    modified = CGAffineTransform.nextRandom()
-                }
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform(scaleX: 2, y: 3)]}
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform.identity]}
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform(translationX: 100, y: 200)]}
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform(translationX: 100, y: 200).scaledBy(x: 2, y: 3)]}
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform(scaleX: 2, y: 3).translatedBy(x: 200, y: 300)]}
+                itBehavesLike("reverts to identity") {["transform":CGAffineTransform.nextRandom()]}
             }
         }
     }
