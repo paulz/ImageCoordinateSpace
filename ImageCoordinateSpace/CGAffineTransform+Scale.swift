81bc0c77caa873fb9b4be53d0ce6c2315d61b0d7
Paul Zabelin <pzabelin+git@gmail.com>
2019-03-10 19:32:15 -0700

    extract CGAffineTransform extension methods

--- ImageCoordinateSpace/CGAffineTransform+Scale.swift
+++ ImageCoordinateSpace/CGAffineTransform+Scale.swift
@@ -23,4 +23,12 @@ extension CGAffineTransform {
         self.init(scaleX: size.width / from.width,
                   y: size.height / from.height)
     }
+
+    init(translation size: CGSize) {
+        self.init(translationX: size.width, y: size.height)
+    }
+
+    func scaledBy(_ scale: CGFloat) -> CGAffineTransform {
+        return scaledBy(x: scale, y: scale)
+    }
 }
--- ImageCoordinateSpace/SizeTransformer.swift
+++ ImageCoordinateSpace/SizeTransformer.swift
@@ -19,19 +19,19 @@ struct SizeTransformer {
         return CGAffineTransform(scaleTo: boundsSize, from: contentSize)
     }
 
-    private func translateAxies(by: ScaleFactor, sizeScale scale: CGFloat, path: KeyPath<CGSize, CGFloat>) -> CGFloat {
-        return by.scale(value: boundsSize[keyPath: path] - contentSize[keyPath: path] * scale)
+    private func translateAxies(by factor: ScaleFactor, sizeScale scale: CGFloat, path: KeyPath<CGSize, CGFloat>) -> CGFloat {
+        return factor.scale(value: boundsSize[keyPath: path] - contentSize[keyPath: path] * scale)
     }
 
     func translate(factor: SizeFactor, sizeScale scale: CGFloat = 1.0) -> CGAffineTransform {
-        var result = CGSize()
+        var translation = CGSize()
         [\CGSize.width: factor.width,
          \CGSize.height: factor.height
             ].forEach {
                 let (path, factor) = $0
-                result[keyPath:path] = translateAxies(by: factor, sizeScale: scale, path: path)
+                translation[keyPath:path] = translateAxies(by: factor, sizeScale: scale, path: path)
         }
-        return CGAffineTransform(translationX: result.width, y: result.height)
+        return CGAffineTransform(translation: translation)
     }
 
     func translateAndScale(using reduceFunction: (CGFloat, CGFloat) -> CGFloat) -> CGAffineTransform {
@@ -39,6 +39,6 @@ struct SizeTransformer {
             let fill = scaleToFill()
             return reduceFunction(fill.scaleX, fill.scaleY)
         }()
-        return translate(factor: SizeFactor(), sizeScale: scale).scaledBy(x: scale, y: scale)
+        return translate(factor: SizeFactor(), sizeScale: scale).scaledBy(scale)
     }
 }
